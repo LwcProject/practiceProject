96ef235a19c34cdd7c97020a930b324b6e7750b2
lwcReber <1079670310@qq.com>
2018-01-30 22:47:40 +0800

    添加公共函数

--- JS/CommonLib/Util.js
+++ JS/CommonLib/Util.js
@@ -153,7 +153,7 @@ var Util = {
 
     return jsonObj;
   },
-  
+
   /**
    * inputNum - input输入框限制输入类型
    *
@@ -167,5 +167,26 @@ var Util = {
         val = val.replace(/[^\d]/g, '');//非数字的都替换掉
         val = val.substr(0, len);
         t.value = val;
+  },
+  /**
+ * val - 提交表单验证
+ *
+ * @param  {String} formId   表单id名
+ * @return {Object}          返回该表单是否验证结果 flag为true验证通过
+ */
+  vali (formId) {
+    var msg = '', flag = true, vObj = $('.validate');
+    if (formId) {
+      vObj = $('#' + formId).find('.validate');
+    }
+    for (var i = 0; i < vObj.length; i++) {
+        var t = vObj.eq(i);
+        if (!t.val()) {
+            msg = t.attr("placeholder");
+            flag = false;
+            break;
+        }
+    }
+    return { msg: msg, flag: flag };
   }
 }

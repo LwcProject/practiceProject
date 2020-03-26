e264bb6045c47a55cc5cffda0c135f260d80c94c
LwcReber <1079670310@qq.com>
2020-03-27 00:00:21 +0800

    promise

---
+++ JS/promise.js
@@ -0,0 +1,74 @@
+class RPromise {
+  constructor (fn) {
+    let self = this
+    this.status = 'padding'
+    this.value = null
+    this.error = null
+    this.resolveCallbacks = [] // 所有的resolve回调
+    this.rejectCallbacks = [] // 所有的resolve回调
+    let resolve = function (value) {
+      setTimeout( () => {
+        self.value = value
+        self.status = 'fulfilled'
+        self.resolveCallbacks.map(cb => cb(this.value))
+      }, 0)
+    }
+
+    let reject = function (error) {
+      setTimeout(() => {
+        self.error = error
+        self.status = 'rejected'
+        self.rejectCallbacks.map(ca => cb(this.error))
+      })
+    }
+    try {
+      fn(resolve, reject)
+    } catch (e) {
+      reject(e)
+    }
+  }
+
+  static then (resolvecb, rejectedcb) {
+
+  }
+
+  static catch () {
+
+  }
+  static all () {
+
+  }
+
+  static reace () {
+
+  }
+
+  static resolve () {
+    if (this.status === 'padding') {
+      this.status = 'fulfilled'
+    }
+  }
+  static reject () {
+    if (this.status === 'padding') {
+      this.status = 'rejected'
+    }
+  }
+
+  then () {
+
+  }
+
+  catch () {
+
+  }
+
+  finally () {
+
+  }
+}
+var a = new RPromise(function(resolve, reject) {
+  let dd = 0
+  if (dd === 0) {
+    resolve(dd)
+  }
+})
